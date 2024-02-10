d689fc844496fd23a461d41d797d3b8a781f4d9e
tamij12 <tamijangosrz@gmail.com>
2024-02-09 20:44:52 -0600

    beneficios-section

--- src/app/landing/page.js
+++ src/app/landing/page.js
@@ -9,7 +9,7 @@ export default function landing() {
       <div className="w-80">
         <main className="mt-6">
           <div className="title-hero">
-            <div className="font-josefin-regular text-2xl font-bold flex justify-center px-8">
+            <div className="font-josefin-regular text-2xl font-extrabold flex justify-center px-8">
               <p className="">Descubre</p>
               <p className="text-violetVitalli  pl-2">VitalliApp</p>
             </div>
@@ -29,6 +29,52 @@ export default function landing() {
             />
           </div>
         </main>
+
+        <section>
+          <div className="flex justify-center">
+            <p className="font-josefin-regular font-bold text-[#6F2ADF]">
+              Beneficios
+            </p>
+          </div>
+
+          <div className="mt-6 mr-4 flex justify-end font-lato-regular">
+            <div className="card shadow-xl w-64 bg-[#DFEAFF] text-[#4D3AC9] flex-row">
+              {/* <div className="flex">
+                <img src="/botiquin.svg" />
+              </div> */}
+              <div className="card-body">
+                <p>
+                  Guarda tu infomacion medica importante para no perderla de
+                  vista.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-6 ml-4 flex justify-start font-lato-regular">
+            <div className="card shadow-xl  w-64 bg-[#F4F2FF] text-[#4D11AF] flex-row">
+              {/* <div className="flex">
+                <img src="/receta.svg" />
+              </div> */}
+              <div className="card-body ">
+                <p>
+                  Manten el seguimiento de tus tratamientos y citas medicas.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-6 mr-4 flex justify-end font-lato-regular">
+            <div className="card shadow-xl w-64 bg-[#D7FBED] text-[#263238] flex-row">
+              {/* <div className="flex">
+                <img src="/tanque-oxigeno.svg" width={300}/>
+              </div> */}
+              <div className="card-body">
+                <p>Mejora la atencion de tu salud por tu propia cuenta.</p>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </React.Fragment>
   );

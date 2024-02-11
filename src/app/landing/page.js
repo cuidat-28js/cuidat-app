50ebd2bb12d792a1e55d54a8942f3dbc59574ec6
Daniel Arce <alarto96@gmail.com>
2024-02-10 22:48:48 -0600

    components in landing

--- src/app/landing/page.js
+++ src/app/landing/page.js
@@ -3,13 +3,17 @@ import React from "react";
 import "./landing.css";
 import Benefits from "../components/landing-components/Benefits";
 import HeroSection from "../components/landing-components/HeroSection";
+import NavbarLan from "../components/landing-components/Navbar-landing";
+import Footer from "../components/landing-components/Footer";
 
 export default function landing() {
   return (
     <React.Fragment>
       <div className="w-80">
+        <NavbarLan/>
         <HeroSection />
         <Benefits />
+        <Footer/>
       </div>
     </React.Fragment>
   );

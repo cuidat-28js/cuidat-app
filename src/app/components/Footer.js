60fe94fc1a7baf22326e057f4d04b6fc15f498c0
Daniel Arce <alarto96@gmail.com>
2024-02-10 19:50:14 -0600

    adjustments to navbar footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -3,14 +3,12 @@ import Link from "next/link"
 
 function Footer() {
     return(
-        <div className="section-footer">
-            <div className="text-center py-4" >
-                <Link>
-                    <Image src="/logo-lg.svg"></Image>
-                </Link>
-                <p>VitaliApp 2024 | Todos los derechos reservados</p>
-            </div>
-        </div>
+        <footer class="footer footer-center p-3 bg-primary text-primary-content">
+            <aside>
+                <Image src="/logo-lg.svg" width="100" height="100" viewBox="0 0 24 24"/>
+                <p>Copyright © 2024 - All right reserved</p>
+            </aside>
+        </footer>
     )
 }
 
--- src/app/landing/page.js
+++ src/app/landing/page.js
@@ -7,7 +7,7 @@ export default function landing() {
   return (
     <React.Fragment>
       <div className="w-80">
-        <main className="mt-6">
+        <main className="mt-16">
           <div className="title-hero">
             <div className="font-josefin-regular text-2xl font-bold flex justify-center px-8">
               <p className="">Descubre</p>

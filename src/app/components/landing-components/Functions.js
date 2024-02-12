2999cd55f006e3e789ffb8f8237808a698d14bf5
Daniel Arce <alarto96@gmail.com>
2024-02-11 21:27:08 -0600

    functionalitis added pics

--- src/app/components/landing-components/Functions.js
+++ src/app/components/landing-components/Functions.js
@@ -1,25 +1,29 @@
 import React from "react";
+import Image from "next/image"
 
 export default function Functions() {
     return(
         <React.Fragment>
-            <div className="flex justify-content">
-                <h3 className="font-josefin-regular font-bold text-[#6F2ADF]">Funcionalidades</h3>
-                <div>
-                    <div className="card flex w-96 bg-base-100 ">
-                        <div className="card-body"> 
+            <div className="justify-center mt-5">
+                <h2 className="font-josefin-regular text-center font-bold text-[#6F2ADF]">Funcionalidades</h2>
+                <div className="mt-5">
+                    <div className="card flex w-96 bg-base-80 ">
+                        <div className="card-body">
+                        <Image src="/icons/calendar.svg" height={50} width={50} />
                         <h4 className="card-title text-primary">Agenda para citas</h4>
                         <p>Puedes registrar tus citas y avances sobre tu salud</p>
                         </div>
                     </div>
-                    <div className="card w-96 bg-base-100 shadow-xl">
+                    <div className="card w-96 bg-base-80 shadow-xxl">
                         <div className="card-body">
+                        <Image src="/icons/lista.svg" height={50} width={50} />
                         <h4 className="card-title text-primary">Historial Medico</h4>
                         <p>Tener a la mano tu perfil medico es muy importante</p>
                         </div>
                     </div>
-                    <div className="card w-96 bg-base-100 shadow-xl">
+                    <div className="card w-96 bg-base-80 ">
                         <div className="card-body">
+                        <Image src="/icons/location.svg" height={50} width={50} />
                         <h4 className="card-title text-primary">Localiza un especialista</h4>
                         <p>Encuentra facilmente un especialista de la salud cerca de ti</p>
                         </div>

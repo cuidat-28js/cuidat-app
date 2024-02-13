e8695d7565998465e47acf6d2e3a6197f6a283d3
Daniel Arce <alarto96@gmail.com>
2024-02-13 11:21:50 -0600

    fixed page error

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
--lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+
 import Image from "next/image";
 import Navbar from "./components/Navbar"
 import Footer from "./components/landing-components/Footer";

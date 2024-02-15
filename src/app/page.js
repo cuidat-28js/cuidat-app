090d76620157d6a48c4789e77ffc60aaacc6e25b
Daniel Arce <alarto96@gmail.com>
2024-02-14 21:34:57 -0600

    Revert "made component add button"
    This reverts commit 2121f4bf52f846a36ad289064e41f0482830cd83.

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,11 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar"
 import Footer from "./components/landing-components/Footer";
-import AddBtn from "./components/add-button";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-50">
       <Navbar/>
-      <AddBtn/>
       <div className=" max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1>ESTE ES EL HOME</h1>
       </div>

b04834f15423016fd3035e90339344978fef07b5
Daniel Arce <alarto96@gmail.com>
2024-02-14 22:05:44 -0600

    changes in home

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar"
 import Footer from "./components/landing-components/Footer";
+import AddBtn from "./components/Add-button";
 
 export default function Home() {
   return (
@@ -21,22 +22,17 @@ export default function Home() {
             </div>
         </div>
         <div className="apointments m-4">
-          <div className="m-2 md:m-6 flex">
+          <div className="mb-2 md:m-6 flex">
             <h3 className="font-josefin-regular text-lg mt-2 mr-2">Citas</h3>
-            <div className="pt-2 relative mx-auto text-gray-600">
+            <div className="pt-2 mr-3 relative mx-auto text-gray-600">
               <input className="border-2 border-gray-300 bg-white h-8 px-5 pr-2 rounded-full text-sm focus:outline-none"
               type="search" placeholder="Buscar"/>
             </div>
-            <button
-              type="button"
-              class="ml-3 inline-block rounded border-2 border-primary px-3.5 pb-[6px] pt-2 text-lg leading-normal text-primary hover:border-3 hover:bg-primary hover:text-white"
-              data-te-ripple-init>
-              +
-            </button>
+            <AddBtn/>
           </div>
           <div className="justify-center rounded-lg justify-center shadow bg-[#F9F9F9] p-3">
               <Image src="/ilustrations/doctora-record.svg"
-                width={300}
+                width={200}
                 height={200}/>
               <p className="mb-1">
                 Aun no tienes registros de citas

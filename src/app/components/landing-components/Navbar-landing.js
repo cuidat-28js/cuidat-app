cd9f1be6c1e1b114178913cbc7bde6f6ddf9ee5e
tamij12 <tamijangosrz@gmail.com>
2024-02-15 23:12:45 -0600

    changes

--- src/app/components/landing-components/Navbar-landing.js
+++ src/app/components/landing-components/Navbar-landing.js
@@ -1,10 +1,12 @@
-"use client"
-import Image from 'next/image';
-import Link from 'next/link';
-import { useState } from 'react';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 function NavBar() {
   const [navbar, setNavbar] = useState(false);
+  const router = useRouter();
   return (
     <div>
       <nav className="shadow-lg shadow-[#E7E3FF] w-full bg-white fixed top-0 left-0 right-0 z-10">
@@ -12,11 +14,8 @@ function NavBar() {
           <div>
             <div className="flex items-center justify-between py-1 md:py-5 md:block">
               {/* LOGO */}
-              <Link href="/" className='mt-0'>
-                <Image src="/logo-lg.svg" 
-                    width={100}
-                    height={30}
-                    alt="logo" />
+              <Link href="/" className="mt-0">
+                <Image src="/logo-lg.svg" width={100} height={30} alt="logo" />
               </Link>
               {/* HAMBURGER BUTTON FOR MOBILE */}
               <div className="md:hidden">
@@ -25,12 +24,17 @@ function NavBar() {
                   onClick={() => setNavbar(!navbar)}
                 >
                   {navbar ? (
-                    <Image src="/icons/close-menu.svg" width={30} height={30} alt="logo" />
+                    <Image
+                      src="/icons/close-menu.svg"
+                      width={25}
+                      height={25}
+                      alt="logo"
+                    />
                   ) : (
                     <Image
                       src="/icons/burger-btn.svg"
-                      width={30}
-                      height={30}
+                      width={25}
+                      height={25}
                       alt="logo"
                       className="focus:border-none active:border-none"
                     />
@@ -42,34 +46,37 @@ function NavBar() {
           <div>
             <div
               className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-                navbar ? 'p-12 md:p-0 block' : 'hidden'
+                navbar ? "p-12 md:p-0 block" : "hidden"
               }`}
             >
-              <ul className="h-screen md:h-auto items-center justify-center md:flex ">
-                <li className="pb-2 text-l font-josefin-regular text-black py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:border-primary  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+              <ul className="h-screen md:h-auto items-center justify-start md:flex ">
+                <li className="pb-2  font-lato-regular text-black py-2 hover:text-violetVitalli hover:font-bold md:px-6 text-center md:border-b-0 md:hover:text-violetVitalli md:hover:bg-transparent md:hover:font-bold md:text-xs lg:text-lg">
                   <Link href="#about" onClick={() => setNavbar(!navbar)}>
-                    ¿Que es?
+                    ¿Que ofrecemos?
                   </Link>
                 </li>
-                <li className="pb-62 text-l font-josefin-regular text-black py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                <li className="pb-62  font-lato-regular text-black py-2 md:px-6 text-center hover:text-violetVitalli hover:font-bold md:border-b-0 md:hover:text-violetVitalli md:hover:bg-transparent md:hover:font-bold md:text-xs lg:text-lg">
                   <Link href="#about" onClick={() => setNavbar(!navbar)}>
                     Funcionalidades
                   </Link>
                 </li>
-                <li className="pb-2 text-l font-josefin-regular text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                <li className="pb-2  font-lato-regular text-black py-2 px-6 text-center hover:text-violetVitalli hover:font-bold md:border-b-0 md:hover:text-violetVitalli md:hover:bg-transparent md:hover:font-bold md:text-xs lg:text-lg">
                   <Link href="#blog" onClick={() => setNavbar(!navbar)}>
                     Beneficios
                   </Link>
                 </li>
-                <li className="pb-2 text-l font-josefin-regular text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                  <Link href="#contact" onClick={() => setNavbar(!navbar)}>
+                <li className="pb-2  font-lato-regular text-violetVitalli py-2 px-6 text-center hover:text-violetVitalli hover:font-bold md:border-b-0 border-purple-900 md:hover:font-bold md:hover:bg-transparent md:text-xs lg:text-lg">
+                  <Link href="/login" onClick={() => setNavbar(!navbar)}>
                     Inicia Sesion
                   </Link>
                 </li>
-                <li className="pb-2 text-l font-josefin-regular text-white py-2 px-6 text-center bg-violetVitalli rounded-full border-b-2 md:border-b-0 hover:border-purple hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                  <Link href="#projects" onClick={() => setNavbar(!navbar)}>
-                    Registrate
-                  </Link>
+                <li className="pb-2 text-l font-lato-regular py-2 px-2 text-center md:border-b-0 md:hover:text-violetVitalli md:hover:bg-transparent">
+                  <button
+                    className="btn rounded-full bg-[#4D11AF]  text-[#ffff] hover:bg-[#4D11AF] hover:shadow-[#4D11AF] hover:border-[#4D11AF] md:text-xs lg:text-lg"
+                    onClick={() => router.push("/signin")}
+                  >
+                    ¡Registrate!
+                  </button>
                 </li>
               </ul>
             </div>

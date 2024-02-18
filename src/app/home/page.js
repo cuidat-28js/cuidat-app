d9abc05b8f9a5003ca3eebb59a5d70c1acfbd6ad
tamij12 <tamijangosrz@gmail.com>
2024-02-18 17:46:46 -0600

    home-platform

---
+++ src/app/home/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "../components/Navbar";
+
+export default function Home() {
+    return(
+        <React.Fragment>
+            <NavBar/>
+            
+        </React.Fragment>
+    )
+}

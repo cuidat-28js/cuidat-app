39647f1bbe018dc0f981320e635e288fe89f258d
Daniel Arce <alarto96@gmail.com>
2024-02-18 19:29:35 -0600

    new empty components

---
+++ src/app/components/componentsEmpty/MsgRecord.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function MsgRecord() {
+    return(
+        <React.Fragment>
+            <div className="bg-[#F9F9F9] border rounded-lg m-2 py-3 w-56">
+                <p className="text-sm text-center">
+                    Añade informacion a tu expediente medico
+                </p>
+            </div>
+        </React.Fragment>
+    )
+}

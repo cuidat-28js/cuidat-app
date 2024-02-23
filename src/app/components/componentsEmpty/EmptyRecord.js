015587ce604813f1459ec97b9f11aed43815e35b
Daniel Arce <alarto96@gmail.com>
2024-02-23 04:06:12 -0600

    new record form

--- src/app/components/componentsEmpty/EmptyRecord.js
+++ src/app/components/componentsEmpty/EmptyRecord.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function MsgRecord() {
+export default function EmptyRecord() {
     return(
         <React.Fragment>
-            <div className="bg-[#F9F9F9] border rounded-lg m-2 py-3 w-56">
+            <div className="bg-[#F9F9F9] border rounded-lg m-1 py-3 w-52">
                 <p className="text-sm text-center">
                     Añade informacion a tu expediente medico
                 </p>

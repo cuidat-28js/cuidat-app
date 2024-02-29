7eae053428261084ce89b14f1fb02e339dbe32df
Daniel <danielrmmata@gmail.com>
2024-02-28 22:31:13 -0600

    fix-modalReminder

--- src/app/components/newAppointment-components/Form-newappointment.js
+++ src/app/components/newAppointment-components/Form-newappointment.js
@@ -22,7 +22,7 @@ export default function FormNewAppointment() {
           onSubmit={handleSubmit((data) => {
             console.log(data);
           })}
-          className="bg-white px-10 py-20 rounded-3xl border-gray-100 mt-8 shadow-2xl"
+          className="bg-white px-10 py-20 rounded-3xl border-gray-100 mt-8 shadow-xl shadow-violet-500"
         >
           <h1 className="text-3xl font-semibold text-center">Nueva cita</h1>
           <div className="mt-8 max-w-96 space-y-8 mx-auto">
@@ -35,7 +35,7 @@ export default function FormNewAppointment() {
               </label>
               <input
                 id="date-appointment"
-                type="date"
+                type="datetime-local"
                 className="w-full py-2 border-2 rounded-md p-4 mt-1 bg-transparent border-violet-800 shadow-md"
                 {...register("date", {
                   required: "*Éste campo es obligatorio",
@@ -161,100 +161,7 @@ export default function FormNewAppointment() {
         </form>
       </div>
       <ModalReminder isVisible={showModal} onClose={() => setShowModal(false)}>
-        <div className=" flex flex-col w-160 m-2">
-          <div className=" self-center ml-6 ">
-            <h3 className="font-semibold">Añadir Recordatorio</h3>
-          </div>
-          <div className="flex flex-col md:flex-row pl-2">
-            <div className="w-56 my-4">
-              <div>
-                <div className="mb-2">
-                  <label
-                    for="large-input"
-                    className="block text-sm font-medium"
-                  >
-                    Titulo
-                  </label>
-                  <input
-                    type="text"
-                    id="inputTitle"
-                    className="w-full border-2 rounded-md px-2 mt-1 bg-transparent border-violet-800 shadow-md"
-                  />
-                </div>
-                <div className="mb-2">
-                  <label
-                    for="large-input"
-                    className="block text-sm font-medium"
-                  >
-                    Fecha
-                  </label>
-                  <input
-                    type="date"
-                    id="inputDate"
-                    className="w-full border-2 rounded-md px-2 mt-1 bg-transparent border-violet-800 shadow-md"
-                  />
-                </div>
-              </div>
-              <div className="mt-5">
-                <button className="w-full active:scale-[.98] active:duration-75 hover:scale-[1.01] easy-in-out transition-all py-2 rounded-xl bg-violet-500 text-white text-sm font-medium">
-                  Añadir
-                </button>
-              </div>
-            </div>
-            <div className="ml-4 flex flex-row">
-              <div className="m-1 w-20">
-                <p className="mt-3 text-sm font-medium">Repetir</p>
-                <div className="flex flex-col">
-                  <label className=" mt-2 text-xs">
-                    <input type="radio" name="recurrence" value="1hr" /> 1 hr
-                  </label>
-                  <label className=" mt-2 text-xs">
-                    <input type="radio" name="recurrence" value="2hr" /> 2 hrs
-                  </label>
-                  <label className=" mt-2 text-xs">
-                    <input type="radio" name="recurrence" value="4hr" /> 4 hrs
-                  </label>
-                  <label className=" mt-2 text-xs">
-                    <input type="radio" name="recurrence" value="6hr" /> 6 hrs
-                  </label>
-                  <label className=" mt-2 text-xs">
-                    <input type="radio" name="recurrence" value="8hr" /> 8 hrs
-                  </label>
-                  <label className=" mt-2 text-xs">
-                    <input type="radio" name="recurrence" value="12hr" /> 12 hrs
-                  </label>
-                </div>
-              </div>
-              <div className="mb-1 mt-2 w-28">
-                <p className="mt-2 text-sm font-medium">Notificación</p>
-                <div>
-                  <div className="form-control">
-                    <label className="label cursor-pointer">
-                      <span className="label-text text-xs">E-mail</span>
-                      <input
-                        type="checkbox"
-                        className="checkbox checkbox-xs checkbox-primary"
-                      />
-                    </label>
-                  </div>
-                  <div className="form-control">
-                    <label className="label cursor-pointer">
-                      <span className="label-text text-xs">
-                        Notificacion
-                        <br />
-                        VitaliApp
-                      </span>
-                      <input
-                        type="checkbox"
-                        className="checkbox checkbox-xs checkbox-primary"
-                      />
-                    </label>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+
       </ModalReminder>
     </Fragment>
   );

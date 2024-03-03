2b6de19e89d2aa1b06a7ebd1e7a44425d3008e30
Daniel Arce <alarto96@gmail.com>
2024-03-02 18:53:23 -0600

    bg and shadow changes in record

--- src/app/recordForm/page.js
+++ src/app/recordForm/page.js
@@ -3,8 +3,7 @@ import React from "react"
 import { useState } from "react";
 import Navbar from  "../components/Navbar"
 import AddBtn from "../components/Add-button"
-import Link from "next/link"
-import { useState } from "react";
+import Footer from "../components/landing-components/Footer";
 
 export default function RecordForm() {
 
@@ -15,19 +14,20 @@ export default function RecordForm() {
     };
   
     return (
-    <div id="recordForm" className="section relative pt-16 md:pt-16 md:pb-0 bg-white">
+    <div id="recordForm" className="flex flex-col min-h-screen section relative pt-16 md:pt-16 md:pb-0 bg-white">
     <Navbar/>
-        <div className="container mt-2 lg:max-w-6xl mx-auto lg:px-8">
+    
+        <div className="container mt-2 md:mb-22 lg:max-w-6xl mx-auto lg:px-8">
                 <div className="flex items-center justify-center md:flex lg:my-4 my-2"> 
                 <h2 className="text-xl font-josefin-regular text-bold">Expediente</h2>
                 <div className="ml-4">
             </div>
         </div>
         
-        <div className="flex flex-wrap flex-row m-1 lg:mx-16 text-center">
+        <div className="flex flex-wrap flex-row m-1 lg:mx-16 text-center shadow-xl shadow-violet-300">
             {/* START block */}
-            <div className="flex-shrink w-full sm:w-1/2 lg:w-1/3 md lg:px-1">
-                <div className="py-3 px-1 h-32 lg:h-44 lg:pr-2 lg:pl-4 bg-gray-50 border-b mb-2 mx-0.5 border-gray-100">
+            <div className="flex-shrink w-full sm:w-1/2 lg:w-1/3 md lg:px-1 bg-violet-200/70">
+                <div className="py-3 px-1 h-32 lg:h-44 lg:pr-2 lg:pl-4 border-b mb-2 mx-0.5 bg-white">
                     <div className="flex flex-row mb-2 lg:my-2">
                         <h3 className="ml-2 text-md lg:text-lg font-josefin-regular text-black">Grupo Sanguineo</h3>
                     </div>
@@ -104,8 +104,8 @@ export default function RecordForm() {
                 </div>
             </div>
             {/* END block */}
-            <div className="flex-shrink px-1 w-full sm:w-1/2 lg:w-1/3 lg:px-1">
-                <div className="py-3 px-1 h-32 lg:h-44 lg:px-2 bg-gray-50 border-b mb-2 mx-0.5 border-gray-100">
+            <div className="flex-shrink px-1 w-full sm:w-1/2 lg:w-1/3 lg:px-1 bg-violet-200/70">
+                <div className="py-3 px-1 h-32 lg:h-44 lg:px-2 bg-white border-b mb-2 mx-0.5">
                     <div className="justify-between flex flex-row mb-2 lg:my-2">
                         <h3 className="text-md lg:text-lg font-josefin-regular text-black">Alergias</h3>
                         <div className="mr-1">
@@ -116,8 +116,8 @@ export default function RecordForm() {
                 </div>
             </div>
             {/* END block */}
-            <div className="flex-shrink px-1 w-full sm:w-1/2 lg:w-1/3 lg:px-1">
-                <div className="py-3 px-1 h-32 lg:h-44 lg:px-2 bg-gray-50 border-b mb-2 mx-0.5 border-gray-100">
+            <div className="flex-shrink px-1 w-full sm:w-1/2 lg:w-1/3 lg:px-1 bg-violet-200/70">
+                <div className="py-3 px-1 h-32 lg:h-44 lg:px-2 bg-white border-b mb-2 mx-0.5">
                     <div className="flex flex-row justify-between mb-2 lg:my-2">
                         <h3 className="text-md mr-6 lg:text-lg font-josefin-regular text-black">Enfermedades Cronicas</h3>
                         <div className="mr-1">
@@ -128,20 +128,20 @@ export default function RecordForm() {
                 </div>
             </div>
             {/* END block */}
-            <div className="flex-shrink w-full sm:w-1/2 lg:w-1/3 lg:px-1">
-                <div className="py-3 px-1 h-32 lg:h-44 lg:px-2 bg-gray-50 border-b mb-2 mx-0.5 border-gray-100">
+            <div className="flex-shrink w-full sm:w-1/2 lg:w-1/3 lg:px-1 bg-violet-200/70">
+                <div className="py-3 px-1 h-32 lg:h-44 lg:px-2 bg-white border-b mb-2 mx-0.5">
                     <div className="flex flex-row justify-between mb-2 lg:my-2">
                         <h3 className="ml-2 text-md lg:text-lg font-josefin-regular text-black">Medicamento Controlado</h3>
                         <div className="mr-1">
                             <AddBtn />
                         </div>
                     </div>
-                    <input type="text" placeholder="Type here" className="mt-2 input input-xs lg:input-sm input-bordered input-primary w-full max-w-xs" />
+                    <input type="text" placeholder="Clonazepan" className="mt-2 input input-xs lg:input-sm input-bordered input-primary w-full max-w-xs" />
                 </div>
             </div>
             {/* END block */}
-            <div className="flex-shrink w-full sm:w-1/2 lg:w-1/3 lg:px-1">
-                <div className="py-3 px-1 h-32 lg:h-44 lg:px-4 bg-gray-50 border-b mb-2 mx-0.5 border-gray-100">
+            <div className="flex-shrink w-full sm:w-1/2 lg:w-1/3 lg:px-1 bg-violet-200/70">
+                <div className="py-3 px-1 h-32 lg:h-44 lg:px-4 bg-white border-b mb-2 mx-0.5">
                     <div className="flex flex-row justify-between mb-2 lg:my-2">
                         <h3 className="text-md lg:text-lg font-josefin-regular text-black">Antecedentes Familiares</h3>
                         <div className="mr-1">
@@ -152,8 +152,8 @@ export default function RecordForm() {
                 </div>
             </div>
             {/* END block */}
-            <div className="flex-shrink w-full sm:w-1/2 lg:w-1/3 lg:px-1">
-                <div className="py-3 px-1 lg:h-44 lg:px-4 lg:pt-14 mb-2 mx-0.5 ">
+            <div className="flex-shrink w-full sm:w-1/2 lg:w-1/3 lg:px-1 bg-violet-200/70">
+                <div className="py-3 px-1 lg:h-44 lg:px-4 lg:pt-14 mb-2 mx-0.5 bg-transparent">
                     <button className="btn btn-sm lg:btn-md btn-primary text-sm lg:text-lg w-1/2 hover:bg-white hover:border-primary hover:text-primary" type="submit">Guardar</button>
                 </div>
                {/* end block */}
@@ -161,6 +161,9 @@ export default function RecordForm() {
         </div>
         {/* end row */}
     </div>
+    {/* <div className="circlePosition w-[590px] h-[400px] bg-[#6851FF] rounded-[100%] absolute z-1 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] blur-[90px]"></div> */}
+    <Footer/>
+    
 </div>
     )
 }

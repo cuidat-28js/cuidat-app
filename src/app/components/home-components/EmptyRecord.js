1f03d0be4e42d77c4027b5d878e1e5a2db12fa6c
Daniel Arce <alarto96@gmail.com>
2024-03-14 00:19:06 -0600

    margin changes

--- src/app/components/home-components/EmptyRecord.js
+++ src/app/components/home-components/EmptyRecord.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function EmptyRecord() {
     return(
         <React.Fragment>
-            <div className="bg-white border  rounded-lg m-1 py-3 w-52">
+            <div className="bg-white rounded-lg m-1 py-3 w-52">
                 <p className="text-sm text-center">
                     Aun no haz completado tu expediente
                 </p>

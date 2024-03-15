de6ef6eb11c8e67b16a10434960c11c9aca02247
Daniel Arce <alarto96@gmail.com>
2024-03-15 14:46:07 -0600

    reminder rename

--- src/app/vitalli/calendar/page.js
+++ src/app/vitalli/calendar/page.js
@@ -7,27 +7,27 @@ import ReminderForm from "../components/Reminder"
 export default function CalendarPage() {
     return (
         <div className="flex min-h-screen lg:w-full flex-col items-center justify-between">
-            <NavBar/>
+            <NavBar />
             <div className="flex flex-shrink justify-center">
-                <div className="flex flex-col justify-between md:w-3/4 mt-20 md:flex-row"> 
+                <div className="flex flex-col justify-between md:w-3/4 mt-20 md:flex-row">
                     <div className="md:w-1/2 flex flex-col m-2 md:mr-4 ">
                         <div className="object-top px-4 pb-4 border border-primary border-l-0 border-t-0 border-r-0">
                             <h2 className="font-josefin-regular text-bold md:text-xl">
-                            Recordatorios
+                                Recordatorios
                             </h2>
                         </div>
                         <div className="mt-4">
                             {/* <section>
                             <RemindersEmpty/>
                             </section> */}
-                            <ReminderPost/>
-                            <ReminderPost/>
-                            <ReminderPost/>
+                            <ReminderPost />
+                            <ReminderPost />
+                            <ReminderPost />
                         </div>
                     </div>
-                    <div className="flex flex-col md:w-1/2 my-4 mx-2"> 
+                    <div className="flex flex-col md:w-1/2 my-4 mx-2">
                         <div className="mb-4 flex flex-shrink">
-                            <CalendarCompon/>
+                            <CalendarCompon />
                         </div>
                         <div>
                             <ReminderForm/>

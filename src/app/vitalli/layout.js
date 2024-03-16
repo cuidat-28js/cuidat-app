f8c75a95da0005723c1b9203212387bd71c3629c
tamij12 <tamijangosrz@gmail.com>
2024-03-15 19:25:09 -0600

    new changes

--- src/app/vitalli/layout.js
+++ src/app/vitalli/layout.js
@@ -1,9 +1,19 @@
 "use client"
 import { QueryClientProvider, QueryClient } from "react-query";
 import { redirect } from "next/navigation";
+import { getUser } from "../auth/hooks/useUser";
+import { useEffect } from "react";
 
 export default function AuthLayout({ children }) {
   const queryClient = new QueryClient();
+  // useEffect(()=>{
+  //   const user = getUser();
+  //   return user;
+  // })
+  
+  // if(!user){
+  //   redirect('/auth/login')
+  // }
   // const isAuthenticated = useIsAuthenticated();
   // if (!isAuthenticated) {
   //   return redirect("/auth/login");

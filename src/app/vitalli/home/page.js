be0d82cc5b105bbb1727af021246caab5236f979
tamij12 <tamijangosrz@gmail.com>
2024-03-20 17:27:59 -0600

    asside

--- src/app/vitalli/home/page.js
+++ src/app/vitalli/home/page.js
@@ -27,7 +27,7 @@ export default function Home() {
           <HomeDates />
         </main>
         <aside className="md:grid md:col-span-3">
-          <NewsAside/>
+          <NewsAsside/>
         </aside>
       </div>
     </div>

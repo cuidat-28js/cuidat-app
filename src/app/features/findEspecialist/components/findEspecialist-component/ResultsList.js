80ccebd385cfa90caef381c85c68d4c554da16df
Daniel <danielrmmata@gmail.com>
2024-03-22 19:04:11 -0600

    feature-mapView

--- src/app/features/findEspecialist/components/findEspecialist-component/ResultsList.js
+++ src/app/features/findEspecialist/components/findEspecialist-component/ResultsList.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react"
-import Data from "./../../../../Shared/Data"
 
 function ResultsList() {
-  const [resultsList, setResultList] = useState(Data.resultsListData)
-  const [selectedResult, setSelectedResult] = useState()
+  /*const [resultsList, setResultList] = useState(Data.resultsListData)
+  const [selectedResult, setSelectedResult] = useState()*/
   return (
     <div>
-      <h2 className="font-bold">Elementos encontrados</h2>
+      {/* <h2 className="font-bold">Elementos encontrados</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {resultsList.map((item, index) => (
           <div className={`flex flex-col justify-center items-center p-2 m-2 rounded-lg cursor-pointer border-[1px] shadow-sm shadow-violet-500 hover:border-violet-800
@@ -16,9 +15,9 @@ function ResultsList() {
             {item.name}
           </div>
         ))}
-      </div>
+      </div> */}
     </div>
-  );
+  )
 }
 
-export default ResultsList;
+export default ResultsList
--- src/app/vitalli/findEspecialist/page.js
+++ src/app/vitalli/findEspecialist/page.js
@@ -1,16 +1,47 @@
-import FindEspecialist from "../../components/findEspecialist-component/FindEspecialist";
-import NavBar from "../../features/home/components/Navbar";
-import Footer from "../../features/landing/components/Footer";
+"use client"
+import React, { useState } from "react"
+import ResultsList from "./../../features/findEspecialist/components/findEspecialist-component/ResultsList"
+import GoogleMapView from "./../../features/findEspecialist/components/findEspecialist-component/GoogleMapView"
 
-
-export default function newAppointment() {
-  
+export default function FindEspecialist() {
+  const [radius, setRadius] = useState(10)
   return (
-    
-    <div>
-        <NavBar />
-        <FindEspecialist />
-        <Footer />
+    <div className="grid grid-cols-4 h-screen mt-12">
+      <div className="p-3">
+        <div class="relative flex">
+          <input
+            type="search"
+            class="w-full border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent block flex-auto border-neutral-200 
+              text-base font-normal focus:border-primary focus:outline-none"
+            placeholder="Search"
+            id="search-Map"
+          />
+          <button
+            class="flex items-center whitespace-nowrap px-3 py-[0.25rem] [&>svg]:h-5 [&>svg]:w-5"
+            id="search-Button"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="p-3">
+          <ResultsList />
+        </div>
+      </div>
+      <div className="col-span-3">
+        <GoogleMapView/>
+      </div>
     </div>
-  )
+  );
 }
